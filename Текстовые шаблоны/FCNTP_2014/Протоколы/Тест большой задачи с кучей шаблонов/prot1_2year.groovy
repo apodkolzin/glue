04b4db3c0902e0e0d20342479000a6af39318305
Andrew F. Podkolzin <apodkolzin@naumen.ru>
2015-04-13 18:18:18 +0300

    test

--- Текстовые шаблоны/FCNTP_2014/Протоколы/Тест большой задачи с кучей шаблонов/prot1_2year.groovy
+++ Текстовые шаблоны/FCNTP_2014/Протоколы/Тест большой задачи с кучей шаблонов/prot1_2year.groovy
@@ -67,7 +67,7 @@ lots.each() {
     lot ->
         openingProtocolLotList.add(getOpeningProtocolLot(lot))
 
-        demands = DemandFcntpHibernateHandler.listDemands1(lot)
+        demands = DemandFcntpHibernateHandler.listDemands(lot)
         demands.addAll(DemandFcntpHibernateHandler.listRejectedDemands(lot))
 
         demands.each() {
